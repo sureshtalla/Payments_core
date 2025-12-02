22107bd44842f83b3f3b25a9f6c2ecd0fed64c78
Ramakrishna <ram.siginam@gmail.com>
2025-12-02 21:37:13 +0530

    Users Service changes

--- Payments_core/Program.cs
+++ Payments_core/Program.cs
@@ -1,7 +1,11 @@
-
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Payments_core.Services.DataLayer;
 using Payments_core.Services.MasterDataService;
 using Payments_core.Services.UserDataService;
+using System.Text;
+//using Payments_core.Services.OtpDataService; 
 
 namespace Payments_core
 {
@@ -12,38 +16,83 @@ namespace Payments_core
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // Register your services
-            builder.Services.AddSingleton<IDapperContext, DapperContext>();
+            // === DATA LAYER & SERVICES REGISTRATION ===
+
+            // Dapper context should be scoped (per request), not singleton
+            builder.Services.AddScoped<IDapperContext, DapperContext>();
+
+            // Master data service
             builder.Services.AddScoped<IMasterDataService, MasterDataService>();
+
+            // User data service (this fixes your error)
             builder.Services.AddScoped<IUserDataService, UserDataService>();
+
+            // OTP service (needed for verify-otp endpoint)
             builder.Services.AddScoped<IOtpService, OtpService>();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            // === CORS ===
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowRCPWClient", policy =>
+                {
+                    policy.WithOrigins(
+                            "http://localhost:4200"   // Angular dev
+                        )
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+
+            // (JWT config can stay commented for now if you’re not using it)
+            /*
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+                };
+            });
+            */
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Static files (if you serve anything from wwwroot)
+            app.UseStaticFiles();
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("AllowRCPWClient");
+
             app.UseHttpsRedirection();
 
+            // app.UseAuthentication(); // uncomment when JWT is enabled
             app.UseAuthorization();
-
 
             app.MapControllers();
 
             app.Run();
+
         }
     }
 }
