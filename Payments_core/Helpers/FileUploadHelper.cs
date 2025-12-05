2ac1235d2fce5c810cb6b3d806ec436d743120c7
sureshtalla <sureshkumar.talla57@gmail.com>
2025-12-05 10:01:00 +0530

    helpers

---
+++ Payments_core/Helpers/FileUploadHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Payments_core.Helpers
+{
+    public static class FileUploadHelper
+    {
+        public static async Task<string?> SaveFileAsync(IFormFile file, string rootFolder)
+        {
+            if (file == null) return null;
+            Directory.CreateDirectory(rootFolder);
+            var ext = Path.GetExtension(file.FileName);
+            var fileName = $"{Guid.NewGuid()}{ext}";
+            var path = Path.Combine(rootFolder, fileName);
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fs);
+            }
+            // Return a relative or public URL in production
+            return path;
+        }
+    }
+}
