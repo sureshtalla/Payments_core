f8012a3fca47f312b77112fc85e0bab2f6195e7e
Ramakrishna <ram.siginam@gmail.com>
2025-12-29 13:31:29 +0530

    Special Price changes

--- Payments_core/Models/PricingMDR.cs
+++ Payments_core/Models/PricingMDR.cs
@@ -83,7 +83,8 @@
         public long user_id { get; set; }
 
         public int Product_Category_Id { get; set; }
-
+        public int ProviderId { get; set; }
+        public int paymentModeId { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
 
@@ -98,6 +99,9 @@
         public string? RoleName { get; set; }
         public string? Merchant { get; set; }
         public string? ProductCategory { get; set; }
+        public string? Providers { get; set; }
+        public string? PaymentMode { get; set; }
+
     }
 
     public class SpecialPriceRequest
@@ -105,10 +109,10 @@
         public long Id { get; set; }
         public long UserId { get; set; }
         public int ProductCategoryId { get; set; }
-
+        public int ProviderId { get; set; }
+        public int PaymentModeId { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
-
         public long ActionBy { get; set; }
     }
 
